5961b416cf9a84091933a4652b26257a577f740b
Arnab Das <arnabdas077@gmail.com>
2017-04-03 15:31:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var xPositions = [];
+var yPositions = [];
+var colors = [];
+
+function setup() {
+    createCanvas(window.outerHeight,window.outerWidth);
+    
+    for(var i = 0; i < 20 ; i++) {
+        xPositions.push(random(0,window.outerWidth));
+        yPositions.push(random(0,window.outerHeight));
+        colors.push(color(random(0,255),random(0,255),random(0,255)));
+    }
+}
+
+function draw() {
+  background(204, 247, 255);
+    
+    mouseClicked = function() {
+       xPositions.push(mouseX);
+       yPositions.push(mouseY);
+       colors.push(color(random(0,255),random(0,255),random(0,255)));
+       
+    };
+       
+    for (var i = 0; i < xPositions.length; i++) {
+         noStroke();
+         fill(colors[i]);
+         ellipse(xPositions[i], yPositions[i], 10, 10);
+         yPositions[i] += 5;
+        
+        if(yPositions[i] > window.outerHeight) {
+          yPositions[i]= 0;
+          xPositions[i] = random(0,window.outerWidth);
+        } 
+    }
+    
+}
